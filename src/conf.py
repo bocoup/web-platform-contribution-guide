ba3ae51349b51c8a36c385448d2ead3f01caec42
Simon Pieters <zcorpan@gmail.com>
2020-05-07 18:56:42 +0200

    Change to a custom CSS file (#100)

--- src/conf.py
+++ src/conf.py
@@ -45,8 +45,9 @@ master_doc = 'index'
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+html_style = 'custom.css_t'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ["_static"]
