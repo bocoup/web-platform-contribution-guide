3bd5226e6b24e97489ad28889e215505c86c65b2
Simon Pieters <zcorpan@gmail.com>
2020-05-18 16:54:05 +0200

    Fix configuration around custom css

--- src/conf.py
+++ src/conf.py
@@ -45,7 +45,6 @@ master_doc = 'index'
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
-html_style = 'custom.css_t'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
